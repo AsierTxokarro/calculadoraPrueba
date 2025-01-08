0888bf428e2c09e8ae898a3f8bde7378ff9f55e0
AsierTxokarro <asiertxokarro@gmail.com>
2025-01-08 13:32:57 +0100

    variables y respuestas

--- Calculadora.java
+++ Calculadora.java
@@ -7,6 +7,20 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
-        int num;
+        double num1, num2, resultado = 0;
+        char operador;
+        boolean operacionValida = true;      
+        
+        System.out.println("Escribe el primer numero: ");
+        num1 = scan.nextDouble();
+        System.out.println("Escribe el segundo numero: ");
+        num2 = scan.nextDouble();
+        System.out.println("Introduce operacion: ( +, -, *, / )");
+        operador = scan.next().charAt(0);
+        
+        if(operador== '+') {
+        	
+        }
+        	
         
     }}
